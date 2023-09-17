6fe6016603d7f160c05738e2b23b61e714fcc5a8
strobelightprojects <mattstrobel17@gmail.com>
2023-09-16 18:39:34 -0600

    prep 2 done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+         Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+
+        int per = int.Parse(answer);
+
+        string letter = "";
+
+        if (per >= 90)
+        {
+            letter = "A";
+        }
+             else if (per >= 80)
+        {
+                letter = "B";
+        }
+                  else if (per >= 70)
+        {
+                    letter = "C";
+        }
+                        else if (per >= 60)
+        {
+                             letter = "D";
+        }
+                             else
+        {
+                                letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+        
+        if (per >= 70)
+        {
+             Console.WriteLine("Congratualtions you passed the class!");
+        }
+            else
+        {
+                 Console.WriteLine("Sorry you didn't pass the class!");
+        }
     }
-}
+ }
