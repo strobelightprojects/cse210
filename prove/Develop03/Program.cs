c37aedd8b33f9671b0ca62a688d012478fcb45b5
strobelightprojects <mattstrobel17@gmail.com>
2023-11-25 00:13:09 -0700

    i give up

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -8,79 +8,50 @@ class Program
     {
         Console.Clear();
 
-        Console.WriteLine("1. Enter a new scripture");
-        Console.WriteLine("2. View and hide words in existing scriptures");
-        Console.WriteLine("Enter your choice (1 or 2): ");
+        
+        Console.WriteLine("1. View and hide words in existing scriptures");
+        Console.WriteLine("Enter your choice (1): ");
         string choice = Console.ReadLine();
 
         List<string> updatedLines = new List<string>(); 
 
-        if (choice == "1")
-        {
-            Console.WriteLine("Enter a scripture reference (e.g., 'Proverbs 3:5-6'): ");
-            string referenceInput = Console.ReadLine();
-
-            Console.WriteLine("Enter the scripture text: ");
-            string scriptureText = Console.ReadLine();
-
-            Console.Clear();
-
-            // Save to csv
-            using (StreamWriter writer = new StreamWriter("scripture.csv", true))
-            {
-                writer.WriteLine(referenceInput);
-                writer.WriteLine(scriptureText);
-            }
-        }
-        else if (choice == "2")
+    
+      if (choice == "1")
         {
             Console.Clear();
             Console.WriteLine("Existing Scriptures:");
 
-            // Read and display 
             string[] lines = File.ReadAllLines("scripture.csv");
 
-            for (int i = 0; i < lines.Length; i += 2)
+            for (int i = 0; i < lines.Length; i++)
             {
-                string reference = lines[i];
-                string text = lines[i + 1];
-
-                Console.WriteLine(reference);
-                Console.WriteLine(text);
+                Console.WriteLine($"{i + 1}. {lines[i]}");
             }
 
             Console.WriteLine("Enter the number of the scripture to hide words for (1, 2, etc.): ");
             string selection = Console.ReadLine();
 
-            if (int.TryParse(selection, out int selectedScripture) && selectedScripture >= 1 && selectedScripture <= lines.Length / 2)
+            if (int.TryParse(selection, out int selectedScripture) && selectedScripture >= 1 && selectedScripture <= lines.Length)
             {
-                string reference = lines[(selectedScripture - 1) * 2];
-                string text = lines[(selectedScripture - 1) * 2 + 1];
+                string selectedLine = lines[selectedScripture - 1];
+                Console.WriteLine($"Selected Scripture: {selectedLine}");
 
-                Console.WriteLine($"Selected Scripture: {reference}");
-                Console.WriteLine(text);
+                // Manipulate the selected scripture - hide words
+                Scripture scripture = new Scripture(selectedLine);
 
-                Console.WriteLine("Do you want to hide words in this scripture? (Y/N): ");
-                string hideChoice = Console.ReadLine();
-
-                if (hideChoice.ToUpper() == "Y")
+                while (!scripture.AllWordsHidden())
                 {
-                    Scripture scripture = new Scripture(text);
+                    Console.Clear();
+                   // Console.WriteLine($"Selected Scripture: {selectedLine}");
+                    Console.WriteLine(scripture.GetScriptureText());
 
-                    while (!scripture.AllWordsHidden())
-                    {
-                        Console.Clear();
-                        Console.WriteLine(reference);
-                        Console.WriteLine(scripture.GetScriptureText());
+                    Console.WriteLine("Press Enter to hide 1-3 words, or type 'quit' to exit:");
+                    string hideWordChoice = Console.ReadLine();
 
-                        Console.WriteLine("Press Enter to hide 1-3 words, or type 'quit' to exit:");
-                        string hideWordChoice = Console.ReadLine();
+                    if (hideWordChoice.ToLower() == "quit")
+                        break;
 
-                        if (hideWordChoice.ToLower() == "quit")
-                            break;
-
-                        scripture.HideRandomWords(1, 3);
-                    }
+                    scripture.HideRandomWords(1,3);
                 }
             }
             else
@@ -92,7 +63,7 @@ class Program
         {
             Console.WriteLine("Invalid choice. Please select 1 or 2.");
         }
-        
+
         File.WriteAllLines("updated_scripture.csv", updatedLines);
     }
 }
